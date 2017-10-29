2a3d1aff2a35db415a18cd14ffd32eaa6a0451a9
Wes <a>
2017-10-29 15:41:50 -0400

    b

--- codeforces-comp/443/diagnosis.cpp
+++ codeforces-comp/443/diagnosis.cpp
@@ -1,3 +1,4 @@
+//http://codeforces.com/contest/879/problem/A
 #include <bits/stdc++.h>
 #define ull unsigned long long
 #define fin for(int i = 0; i < n; i++)
@@ -13,13 +14,21 @@ vi a;
 int n, x;
 q qq;
 
-const int INF = 1e7;
-
-vi a;
-int n, x;
-
 int main() {
     ios::sync_with_stdio(0);
     cin >> n;
+    x = 0;
+    fin {
+        int a,b;
+        cin >> a >> b;
+        int d = a;
+        while(d <= x) {
+            d += b;
+        }
+        x = d;
+    }
+
+    cout << x;
+
     return 0;
 }
